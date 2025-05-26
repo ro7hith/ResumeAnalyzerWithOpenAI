e929d25f80bd5c42a88f93009c256de301fbfa62
ro7hith <159981725+ro7hith@users.noreply.github.com>
2025-05-26 14:33:15 -0500

    Changes

--- BackEnd/backend/src/main/java/com/resumeanalyzer/BackendApplication.java
+++ BackEnd/backend/src/main/java/com/resumeanalyzer/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.resumeanalyzer.backend;
+package com.resumeanalyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
